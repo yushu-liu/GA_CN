d5b32effe08fc4930ae34e7d4d407eca505cbb44
Li Chen <chen1649chenli@gmailcom>
2017-10-15 13:02:33 -0400

    5

--- Project-5/mptcp_sweep.sh
+++ Project-5/mptcp_sweep.sh
@@ -5,7 +5,7 @@ sysctl 'net.mptcp.mptcp_enabled=1'
 #./mptcp.sh topo_measure.py 05 524588 > 05.txt
 ./mptcp.sh topo_measure.py 02 209715 > 02.txt
 #./mptcp.sh topo_measure.py 01 104858 > 01.txt
-./mptcp.sh topo_measure.py 005 52489 > 005.txt
+#./mptcp.sh topo_measure.py 005 52489 > 005.txt
 
 sysctl 'net.mptcp.mptcp_enabled=0'
 
